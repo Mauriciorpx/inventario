8f37c2eab086995e81cb1094885b754cabefb7db
Mauriciorpx <mauricio3741@gmail.com>
2024-05-01 22:07:07 -0500

    Inicializacion de vistas

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<!-- Se usa require para que solo llame 1 vez al archivo -->
+<?php require "./inc/session_start.php"; ?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -5,8 +7,25 @@
 </head>
 <body>
     <?php 
-        include "./inc/navbar.php";
-        include "./inc/script.php";
+
+        if(!isset($_GET['vista']) || $_GET['vista']==""){
+            $_GET['vista']="login";
+        }
+
+        if(is_file("./vistas/".$_GET['vista'].".php") && $_GET['vista']!="login" && $_GET['vista']!="404"){
+            include "./inc/navbar.php";
+            /* Llamada de forma dinamica al archivo que se vaya a usar de la carpeta vistas */
+            include "./vistas/".$_GET['vista'].".php";
+            include "./inc/script.php";
+        }else{
+            if($_GET['vista']=="login"){
+                include "./vistas/login.php";
+            }else{
+                include "./vistas/404.php";
+            }
+        }
+
+        
     ?>
 </body>
 </html>
